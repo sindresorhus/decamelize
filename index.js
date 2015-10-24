edcd953bdeabb6f2a1b45faf4188b763116af239
Abhishek Sharma <portgasd666@gmail.com>
2015-10-24 19:32:22 +0530

    Inlined regex and used single quotes

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ module.exports = function (str, sep) {
 		throw new TypeError('Expected a string');
 	}
 	sep = sep || '_';
-	var regex1 = new RegExp("([a-z\d])([A-Z])", "g");
-	var regex2 = new RegExp("(" + sep + "[A-Z])([A-Z])", "g");
-	return str.replace(regex1, '$1' + sep + '$2')
-					.replace(regex2, '$1' + sep + '$2')
+	return str.replace(/([a-z\d])([A-Z])/g, '$1' + sep + '$2')
+					.replace(new RegExp('(' + sep + '[A-Z])([A-Z])', 'g'), '$1' + sep + '$2')
 					.toLowerCase();
 };
