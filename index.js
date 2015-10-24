b69b7e5636824ff762cf8b2c66001286b7489dff
Abhishek Sharma <portgasd666@gmail.com>
2015-10-24 15:45:33 +0530

    Added support for single letter words

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ module.exports = function (str, sep) {
 	if (typeof str !== 'string') {
 		throw new TypeError('Expected a string');
 	}
-
-	return str.replace(/([a-z\d])([A-Z])/g, '$1' + (sep || '_') + '$2').toLowerCase();
+	sep = sep || '_';
+	var regex1 = new RegExp("([a-z\d])([A-Z])", "g");
+	var regex2 = new RegExp("(" + sep + "[A-Z])([A-Z])", "g");
+	return str.replace(regex1, '$1' + sep + '$2')
+					.replace(regex2, '$1' + sep + '$2')
+					.toLowerCase();
 };
--- test.js
+++ test.js
@@ -7,5 +7,7 @@ test(function (t) {
 	t.assert(decamelize('unicornsAndRainbows') === 'unicorns_and_rainbows');
 	t.assert(decamelize('UNICORNS AND RAINBOWS') === 'unicorns and rainbows');
 	t.assert(decamelize('unicorns-and-rainbows') === 'unicorns-and-rainbows');
+	t.assert(decamelize('thisIsATest') === 'this_is_a_test');
+	t.assert(decamelize('thisIsATest', ' ') === 'this is a test');
 	t.end();
 });
