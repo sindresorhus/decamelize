d088c8ad4a235f2730421d24cf4385ec3f083e02
Sindre Sorhus <sindresorhus@gmail.com>
2017-12-03 11:44:50 +0100

    Meta tweaks

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
-module.exports = (str, sep) => {
-	if (typeof str !== 'string') {
+module.exports = (text, separator) => {
+	if (typeof text !== 'string') {
 		throw new TypeError('Expected a string');
 	}
 
-	sep = typeof sep === 'undefined' ? '_' : sep;
+	separator = typeof separator === 'undefined' ? '_' : separator;
 
-	return str
-		.replace(/([a-z\d])([A-Z])/g, `$1${sep}$2`)
-		.replace(/([A-Z]+)([A-Z][a-z\d]+)/g, `$1${sep}$2`)
+	return text
+		.replace(/([a-z\d])([A-Z])/g, `$1${separator}$2`)
+		.replace(/([A-Z]+)([A-Z][a-z\d]+)/g, `$1${separator}$2`)
 		.toLowerCase();
 };
