cacfc6f11beb214fb21d5cd60537fcbe43919fc5
Corey Farrell <git@cfware.com>
2017-12-03 02:40:26 -0500

    ES2015ify and require Node.js 4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-module.exports = function (str, sep) {
+module.exports = (str, sep) => {
 	if (typeof str !== 'string') {
 		throw new TypeError('Expected a string');
 	}
@@ -7,7 +7,7 @@ module.exports = function (str, sep) {
 	sep = typeof sep === 'undefined' ? '_' : sep;
 
 	return str
-		.replace(/([a-z\d])([A-Z])/g, '$1' + sep + '$2')
-		.replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1' + sep + '$2')
+		.replace(/([a-z\d])([A-Z])/g, `$1${sep}$2`)
+		.replace(/([A-Z]+)([A-Z][a-z\d]+)/g, `$1${sep}$2`)
 		.toLowerCase();
 };
