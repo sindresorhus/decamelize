98ae624de5a246dd086ccfe629101a5234df43d4
Sindre Sorhus <sindresorhus@gmail.com>
2018-01-08 20:08:57 +0100

    Add comment about future regex

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ module.exports = (text, separator) => {
 	const regex2 = xRegExp('(\\p{Lu}+)(\\p{Lu}[\\p{Ll}\\d]+)', 'g');
 
 	return text
+		// TODO: Use this instead of `xregexp` when targeting Node.js 10:
+		// .replace(/([\p{Lowercase_Letter}\d])(\p{Uppercase_Letter})/gu, `$1${separator}$2`)
+		// .replace(/(\p{Lowercase_Letter}+)(\p{Uppercase_Letter}[\p{Lowercase_Letter}\d]+)/gu, `$1${separator}$2`)
 		.replace(regex1, `$1${separator}$2`)
 		.replace(regex2, `$1${separator}$2`)
 		.toLowerCase();
