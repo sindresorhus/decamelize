eda5b7425631a386013b57e263f88dc2d0ea7a7a
Sindre Sorhus <sindresorhus@gmail.com>
2019-07-05 11:56:48 +0700

    Add test for #20

--- test.js
+++ test.js
@@ -10,8 +10,12 @@ test('decamelize', t => {
 	t.is(decamelize('thisIsATest', ' '), 'this is a test');
 	t.is(decamelize('thisIsATest', ''), 'thisisatest');
 	t.is(decamelize('unicornRainbow', '|'), 'unicorn|rainbow');
-	t.is(decamelize('myURLString', '_'), 'my_url_string');
-	t.is(decamelize('URLString', '_'), 'url_string');
-	t.is(decamelize('StringURL', '_'), 'string_url');
 	t.is(decamelize('thisHasSpecialCharactersLikeČandŠ', ' '), 'this has special characters like čand š');
+});
+
+test('handles acronyms', t => {
+	t.is(decamelize('myURLString'), 'my_url_string');
+	t.is(decamelize('URLString'), 'url_string');
+	t.is(decamelize('StringURL'), 'string_url');
+	t.is(decamelize('testGUILabel'), 'test_gui_label');
 });
