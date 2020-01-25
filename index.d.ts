7ac6111f3c44baeb9c3fe793b55681b7c0ac6f90
Corey Farrell <git@cfware.com>
2020-01-25 07:31:51 -0500

    Require Node.js 10 and drop `xregexp` dependency (#22)

--- index.d.ts
+++ index.d.ts
@@ -1,27 +1,20 @@
-declare const decamelize: {
-	/**
-	Convert a camelized string into a lowercased one with a custom separator: `unicornRainbow` → `unicorn_rainbow`.
+/**
+Convert a camelized string into a lowercased one with a custom separator: `unicornRainbow` → `unicorn_rainbow`.
 
-	@param string - The camelcase string to decamelize.
-	@param separator - The separator to use to put in between the words from `string`. Default: `'_'`.
+@param string - The camelcase string to decamelize.
+@param separator - The separator to use to put in between the words from `string`. Default: `'_'`.
 
-	@example
-	```
-	import decamelize = require('decamelize');
+@example
+```
+import decamelize = require('decamelize');
 
-	decamelize('unicornRainbow');
-	//=> 'unicorn_rainbow'
+decamelize('unicornRainbow');
+//=> 'unicorn_rainbow'
 
-	decamelize('unicornRainbow', '-');
-	//=> 'unicorn-rainbow'
-	```
-	*/
-	(string: string, separator?: string): string;
-
-	// TODO: Remove this for the next major release, refactor the whole definition to:
-	// declare function decamelize(string: string, separator?: string): string;
-	// export = decamelize;
-	default: typeof decamelize;
-};
+decamelize('unicornRainbow', '-');
+//=> 'unicorn-rainbow'
+```
+*/
+declare function decamelize(string: string, separator?: string): string;
 
 export = decamelize;
