3f1a84287de99d0c42846c15a1bf0ace02843d52
Fabian Pfannes <fp@maon.eu>
2020-11-27 17:18:50 +0100

    types for perserve consecutive uppercase option

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,26 @@
+declare namespace decamelize {
+	interface Options {
+		/**
+		Set to true to preserve sequences of uppercase characters.
+
+		decamelize('testGUILabel', {preserveConsecutiveUppercase: true}) → `test_GUI_label`
+		decamelize('testGUILabel', {preserveConsecutiveUppercase: false}) → `test_gui_label`
+		@default false
+		*/
+		readonly preserveConsecutiveUppercase?: boolean;
+
+		/**
+		 The separator to use to put in between the words from `string`.
+		@default '_'
+		*/
+		readonly separator?: string;
+	}
+}
+
 /**
 Convert a camelized string into a lowercased one with a custom separator: `unicornRainbow` → `unicorn_rainbow`.
 
 @param string - The camelcase string to decamelize.
-@param separator - The separator to use to put in between the words from `string`. Default: `'_'`.
 
 @example
 ```
@@ -11,10 +29,10 @@ import decamelize = require('decamelize');
 decamelize('unicornRainbow');
 //=> 'unicorn_rainbow'
 
-decamelize('unicornRainbow', '-');
+decamelize('unicornRainbow', { separator: '-' });
 //=> 'unicorn-rainbow'
 ```
 */
-declare function decamelize(string: string, separator?: string): string;
+declare function decamelize(string: string, options?: decamelize.Options): string;
 
 export = decamelize;
