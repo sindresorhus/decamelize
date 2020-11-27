05131f0808106c323a0b511afef15066af3dda65
Fabian Pfannes <fp@maon.eu>
2020-11-27 17:05:10 +0100

    Add more default tests

--- test.js
+++ test.js
@@ -3,6 +3,12 @@ import decamelize from '.';
 
 test('decamelize', t => {
 	t.is(decamelize(''), '');
+	t.is(decamelize('A'), 'a');
+	t.is(decamelize('A B'), 'a b');
+	t.is(decamelize('a2b'), 'a2b');
+	t.is(decamelize('A2B'), 'a2_b');
+	t.is(decamelize('_A2B'), '_a2_b');
+	t.is(decamelize('myURLstring'), 'my_ur_lstring');
 	t.is(decamelize('unicornsAndRainbows'), 'unicorns_and_rainbows');
 	t.is(decamelize('UNICORNS AND RAINBOWS'), 'unicorns and rainbows');
 	t.is(decamelize('unicorns-and-rainbows'), 'unicorns-and-rainbows');
@@ -20,3 +26,8 @@ test('handles acronyms', t => {
 	t.is(decamelize('testGUILabel'), 'test_gui_label');
 	t.is(decamelize('CAPLOCKED1'), 'caplocked1');
 });
+
+test('separator in string', t => {
+	t.is(decamelize('my_URL_string', '_'), 'my_url_string');
+});
+
