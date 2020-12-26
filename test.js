78e8b31fb6d41cdb79cedf117eb3a52052415c0f
Clay <fc@users.noreply.github.com>
2020-12-26 01:51:37 -0800

    Add test corresponding with test in `camelcase` (#28)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- test.js
+++ test.js
@@ -11,6 +11,7 @@ test('decamelize', t => {
 	t.is(decamelize('thisIsATest', ''), 'thisisatest');
 	t.is(decamelize('unicornRainbow', '|'), 'unicorn|rainbow');
 	t.is(decamelize('thisHasSpecialCharactersLikeČandŠ', ' '), 'this has special characters like čand š');
+	/// t.is(decamelize('h2W'), 'h2w');
 });
 
 test('handles acronyms', t => {
