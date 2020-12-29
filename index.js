8b87ee0f07683d2bc561e8d9e51955e6a3463e21
Sindre Sorhus <sindresorhus@gmail.com>
2020-12-29 14:57:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 const handlePreserveConsecutiveUppercase = (decamelized, separator) => {
 	// Lowercase all single uppercase characters. As we
-	// want to preserve uppercase sequences we cannot
-	// simply lowercase the separated string at the end
-	// data_For_USACounties -> data_for_USACounties
+	// want to preserve uppercase sequences, we cannot
+	// simply lowercase the separated string at the end.
+	// `data_For_USACounties` → `data_for_USACounties`
 	decamelized = decamelized.replace(
 		/((?<![\p{Uppercase_Letter}\d])[\p{Uppercase_Letter}\d](?![\p{Uppercase_Letter}\d]))/gu,
 		$0 => {
@@ -12,8 +12,8 @@ const handlePreserveConsecutiveUppercase = (decamelized, separator) => {
 		}
 	);
 
-	// Remaining uppercase sequences will be separated from lowercase sequences
-	// data_For_USACounties -> data_for_USA_counties
+	// Remaining uppercase sequences will be separated from lowercase sequences.
+	// `data_For_USACounties` → `data_for_USA_counties`
 	return decamelized.replace(
 		/(\p{Uppercase_Letter}+)(\p{Uppercase_Letter}\p{Lowercase_Letter}+)/gu,
 		(_, $1, $2) => {
@@ -24,7 +24,10 @@ const handlePreserveConsecutiveUppercase = (decamelized, separator) => {
 
 module.exports = (
 	text,
-	{separator = '_', preserveConsecutiveUppercase = false} = {}
+	{
+		separator = '_',
+		preserveConsecutiveUppercase = false
+	} = {}
 ) => {
 	if (!(typeof text === 'string' && typeof separator === 'string')) {
 		throw new TypeError(
@@ -32,16 +35,16 @@ module.exports = (
 		);
 	}
 
-	// Checking of the second character is done later on. Therefore process shorter strings here
+	// Checking the second character is done later on. Therefore process shorter strings here.
 	if (text.length < 2) {
 		return preserveConsecutiveUppercase ? text : text.toLowerCase();
 	}
 
 	const replacement = `$1${separator}$2`;
 
-	// Split lowercase sequences followed by uppercase character
-	// dataForUSACounties -> data_For_USACounties
-	// myURLstring -> my_URLstring
+	// Split lowercase sequences followed by uppercase character.
+	// `dataForUSACounties` → `data_For_USACounties`
+	// `myURLstring → `my_URLstring`
 	const decamelized = text.replace(
 		/([\p{Lowercase_Letter}\d])(\p{Uppercase_Letter})/gu,
 		replacement
@@ -51,8 +54,8 @@ module.exports = (
 		return handlePreserveConsecutiveUppercase(decamelized, separator);
 	}
 
-	// Split multiple uppercase characters followed by lowercase character(s)
-	// my_URLstring -> my_url_string
+	// Split multiple uppercase characters followed by one or more lowercase characters.
+	// `my_URLstring` → `my_url_string`
 	return decamelized
 		.replace(
 			/(\p{Uppercase_Letter}+)(\p{Uppercase_Letter}\p{Lowercase_Letter}+)/gu,
